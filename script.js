d0c8efaa0f8617fd9dd3ac511dadcb428929bcf3
afaqahmad637151-ship-it <afaqahmmad637151@gmail.com>
2026-09-17 05:32:41 -0700

    mywebsite

--- script.js
+++ script.js
@@ -3,13 +3,47 @@ const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const close = document.getElementById("close");
 
-images.forEach(function(image) {
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+let currentIndex = 0;
+
+
+// Image click
+images.forEach(function(image, index) {
     image.addEventListener("click", function() {
+        currentIndex = index;
         lightboxImg.src = image.src;
         lightbox.style.display = "flex";
     });
 });
 
+
+// Close button
 close.addEventListener("click", function() {
     lightbox.style.display = "none";
+});
+
+
+// Next button
+next.addEventListener("click", function() {
+    currentIndex++;
+
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+
+    lightboxImg.src = images[currentIndex].src;
+});
+
+
+// Previous button
+prev.addEventListener("click", function() {
+    currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+
+    lightboxImg.src = images[currentIndex].src;
 });
